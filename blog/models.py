13885f5292b5fb9672914ce68532fffca15aa966
Andrew <werdnaonaps@gmail.com>
2015-01-18 20:26:54 -0500

    fixed case of 'model' in models.Model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Post(Models.Model):
+class Post(models.Model):
     title_text = models.CharField(max_length = 500)
     pub_date = models.DateTimeField('date published');
     content =  models.TextField();
