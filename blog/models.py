4029c24117ba86dbf53c8cbba2f06eced4a1d3df
Andrew <werdnaonaps@gmail.com>
2015-01-18 20:15:29 -0500

    created model for blog posts

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Post(Models.Model):
+    title_text = models.CharField(max_length = 500)
+    pub_date = models.DateTimeField('date published');
+    content =  models.TextField();
+
+
+    
+
+    
+
+
