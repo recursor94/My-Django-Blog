c149e45fd508fb0fcce38c0c2f3284ce2a9febf6
Andrew <werdnaonaps@gmail.com>
2015-01-18 21:51:30 -0500

    reorganized blog post admin display and registered admin model

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from blog.models import Post
 # Register your models here.
-admin.site.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin):  #customize default admin for blog spots
+    list_display = ('title_text', 'pub_date', 'was_published_recently')
+admin.site.register(Post, PostAdmin)
