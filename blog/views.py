903180a658da5e8787b4db10a02e2ea128054042
Andrew <werdnaonaps@gmail.com>
2015-01-19 00:36:35 -0500

    added templates

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from  blog.models import Post
+from django.template import RequestContext, loader
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world, you're at a test page.")
+    latest_post_list = Post.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('blog/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'blog/index.html', context)
 
 def view_post(request, post_id):
-    return HttpResponse("This is post %s." % post_id)
+    try:
+        post = Post.objects.get(pk=post_id)
+
+    except Post.DoesNotExist:
+        raise Http404("Post does not exist")
+    return render(request, 'blog/posts.html', {'content':post.content})
+    
