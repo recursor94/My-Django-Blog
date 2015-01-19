40081aaa6daf4da9f1df0865488b99c58f1b9ec2
Andrew <werdnaonaps@gmail.com>
2015-01-18 20:58:25 -0500

    added to string to post model

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,8 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 class Post(models.Model):
@@ -7,9 +11,10 @@ class Post(models.Model):
     content =  models.TextField();
 
     #methods
-    def __str__(self):
-        return self.title_text + "\n" self.pub_date + "\n" self.content
-
+    def __unicode__(self):
+        return self.title_text + "\n"+ self.pub_date.__str__() + "\n" + self.content
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1);
 
     
 
