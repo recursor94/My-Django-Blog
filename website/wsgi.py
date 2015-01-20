95fcd0d7e900783914e15ccfdabf882960c1ac4b
Andrew <werdnaonaps@gmail.com>
2015-01-19 19:04:17 -0500

    added whitenoise api and support for static files in heroku

--- website/wsgi.py
+++ website/wsgi.py
@@ -11,8 +11,12 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(get_wsgi_application())
+
+application = get_wsgi_application()
+#application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
