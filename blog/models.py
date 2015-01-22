ae99a4716c2a6ecd0b31f392b0d08c7e80f6f90a
Andrew <werdnaonaps@gmail.com>
2015-01-22 03:35:55 -0500

    added slug field using SlugField method in post model.  The only way to solve issue was to store slug

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,9 @@ from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
     title_text = models.CharField(max_length = 500)
-    pub_date = models.DateTimeField('date published');
-    content =  models.TextField();
+    pub_date = models.DateTimeField('date published')
+    content =  models.TextField()
+    slug = models.SlugField(max_length=100, db_index=True)
 
     #methods
     def __unicode__(self):
