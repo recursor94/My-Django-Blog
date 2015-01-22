7304ec034a3b01a0481ad2cdc0822fe9e65b8326
Andrew <werdnaonaps@gmail.com>
2015-01-22 14:57:26 -0500

    changed user model save function: fixed indentation of super save call so that it called the remainder of the save function if the article already exists

--- blog/models.py
+++ blog/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         if not self.id:
             self.slug = slugify(self.title_text)    #newly created object, set slug
-            super(Post, self).save(*args, **kwargs)
+        super(Post, self).save(*args, **kwargs)
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
