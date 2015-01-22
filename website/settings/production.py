63b5514e2470cffcd2f39dccce1333f4fe8a7ef8
Andrew <werdnaonaps@gmail.com>
2015-01-22 16:40:26 -0500

    production settings added test comment

--- website/settings/production.py
+++ website/settings/production.py
@@ -13,3 +13,4 @@ DATABASES['default'] =  dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#this is a test comment
