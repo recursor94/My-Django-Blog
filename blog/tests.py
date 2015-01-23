5ef84874cdc2c04edef9913779c39a79051c3e12
Andrew <werdnaonaps@gmail.com>
2015-01-23 12:06:39 -0500

    created more tests

--- blog/tests.py
+++ blog/tests.py
@@ -4,8 +4,18 @@ from blog.models import Post
 import datetime
 
 # Create your tests here.
+
+#helper functions
+
+def create_post(title_text, days):
+    """
+    Creates a post with title as title_text and number of days offset to now
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Post.objects.create(title_text=title_text, pub_date=time)
+    
 class PostMethodTests(TestCase):
-    def test_was_published_recently_with_future_question(self):
+    def test_was_published_recently_with_future_post(self):
         """
         was_published_recently() should return False for posts whose
         pub_date is in the future"
@@ -14,7 +24,7 @@ class PostMethodTests(TestCase):
         future_post = Post(pub_date=time)
         self.assertEqual(future_post.was_published_recently(), False)
 
-    def test_was_published_recently_with_distant_past_question(self):
+    def test_was_published_recently_with_distant_past_post(self):
         """
         was published_recently() should return False
         for posts whose pub_date is in the distant future
@@ -23,7 +33,7 @@ class PostMethodTests(TestCase):
         distant_past_post = Post(pub_date=time)
         self.assertEqual(distant_past_post.was_published_recently(), False)
 
-        def test_was_published_recently_with_near_past_question(self):
+        def test_was_published_recently_with_near_past_post(self):
             """
             was published_recently should return True for posts whose pub_date
             was in the recent past--in this case less than or equal to one day
@@ -32,3 +42,17 @@ class PostMethodTests(TestCase):
             time=timezone.now() - datetime(days=1)
             near_past_post = Post(pub_date=time)
             self.assertEqual(distant_past_post.was_published_recently(), True)
+
+        def test_index_view_with_a_future_post(self):
+
+            """Questions with a pub_date in the future should not be
+            displayed on the index page"""
+
+            create_post(question_text="Future Post", days=30)
+            response = self.client.get(reverse('blog:index'))
+            self.assertContains(response, "No posts are available",
+                                status_code = 200)
+            slef.assertQuerysetEqual(response.context['post_list'],
+                                     [])
+            
+
