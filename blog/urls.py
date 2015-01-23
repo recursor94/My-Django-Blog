ab449d5195dcbd4347fa2621496a837623c2fa87
Andrew <werdnaonaps@gmail.com>
2015-01-23 00:14:27 -0500

    fixed navbar base, url and index templates to use navbar as parent

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from blog import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name = 'index'),
+                       url(r'^about/$', views.about, name='about'),
+                       url(r'^contact/$', views.contact, name='contact'),
                        url(r'^(?P<post_slug>[\w-]+)/$', views.view_post, name='view_post'))
